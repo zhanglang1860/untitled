29e8d589b92ff23f0f16711b54926d389e92690e
zhanglang1860 <zhanglang1860@users.noreply.github.com>
2019-01-27 22:49:32 +0800

    testcsv

--- test/taketxt.py
+++ test/taketxt.py
@@ -10,10 +10,10 @@ listglob.sort()
 from PIL import Image
 for filepath in listglob:
     f1 = open(filepath,'r')
-    f2 = open(r'H:\百度云下载\气站数据\30年基准气象站风资源数据\基准气象站风资源数据\car_val1.txt','w')
+    f2 = open(r'H:\百度云下载\气站数据\30年基准气象站风资源数据\基准气象站风资源数据\car_val1.txt','a')
     for line in f1.readlines():
             if re.findall('59265',line): #查找“空格1”的行 每行的格式000005 -1\n 000007  1
                 f2.write(line) #把查找到的行写入f2.
     f1.close()
-    f2.close()
+f2.close()
 
